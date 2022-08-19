ea3472328f46eea5700c7f91f0027c6bac5bdbcf
Henry Frost <henry.90.frost@gmail.com>
2022-08-19 11:49:09 -0400

    added missing file

--- sql-querying-exercise-pt2.sql
+++ sql-querying-exercise-pt2.sql
@@ -42,3 +42,17 @@ WHERE area >= 1000000;
 SELECT SUM(population)
 FROM world
 WHERE name IN  ('Estonia', 'Latvia', 'Lithuania');
+--6
+SELECT continent, COUNT(name)
+FROM world
+GROUP BY continent;
+--7
+SELECT continent, COUNT(name)
+FROM world
+WHERE population >= 10000000
+GROUP BY continent;
+--8
+SELECT continent
+FROM world
+GROUP BY continent
+HAVING SUM(population) >= 100000000;
